c993639e13f3c7a654c002a8b8af0a415b916554
Matthew Jones <matts3371@gmail.com>
2019-11-08 02:59:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,15 @@ def countdown(seconds)
   end
   "HAPPY NEW YEAR!"
 end
+
+#write your code here
+
+def countdown_with_sleep(seconds)
+  seconds = seconds
+  while seconds > 0 
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
